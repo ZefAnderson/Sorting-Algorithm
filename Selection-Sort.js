68632f4d60e00e371d301f1bf28b0a8d986f93cb
Zef Anderson <zeferino@Zefs-MacBook-Pro.local>
2023-03-13 16:07:57 -0600

    got sorting to work

--- Selection-Sort.js
+++ Selection-Sort.js
@@ -1,3 +1,17 @@
-function selectSort(){
+function swap(arr, x, y){
+    let placeHolder = arr[x];
+    arr[x] = arr[y];
+    arr[y] = placeHolder; 
+}
 
+function selectSort(arr, value){
+    let i, j, minIndex;
+
+    for(i = 0; i < value-1; i++){
+        minIndex = i;
+        for(j = i + 1; j < value; j++)
+        if(arr[j] < arr[minIndex])
+            minIndex = j;
+        swap(arr, minIndex, i)
+    }
 }
